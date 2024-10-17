aa5b5722fd5a0e1bc2a5ff84b098155a3109d4fd
Valeria Alarcón <valerialarcon119@gmail.com>
2024-10-17 15:06:51 -0500

    Crud tipo vehiculo

--- src/modulos/tipovehiculo/services/tipo-vehiculo.service.ts
+++ src/modulos/tipovehiculo/services/tipo-vehiculo.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TipoVehiculo } from '../entities/tipoVehiculo.entity';
 
 @Injectable()
-export class TipoVehiculoService {}
+export class TipoVehiculoService {
+    constructor(
+        @InjectRepository(TipoVehiculo) private tipoVehiculoRepo: Repository<TipoVehiculo>
+    ) {}
+
+    ConsultarTiposVehiculo(): Promise<TipoVehiculo[]> {
+        return this.tipoVehiculoRepo.find();
+    }
+
+    InsertarTipoVehiculo(tipoVehiculo: TipoVehiculo): Promise<TipoVehiculo> {
+        const nuevoTipoVehiculo = this.tipoVehiculoRepo.create(tipoVehiculo);
+        return this.tipoVehiculoRepo.save(nuevoTipoVehiculo);
+    }
+
+    async ActualizarTipoVehiculo(id: number, tipoVehiculo: TipoVehiculo): Promise<TipoVehiculo> {
+        const tipoVehiculoActualizar = await this.tipoVehiculoRepo.findOne({ where: { id_tipo: id } });
+        
+        if (!tipoVehiculoActualizar) {
+            throw new Error('No se encontró el tipo de vehículo');
+        }
+
+        this.tipoVehiculoRepo.merge(tipoVehiculoActualizar, tipoVehiculo);
+        return this.tipoVehiculoRepo.save(tipoVehiculoActualizar);
+    }
+}
--- src/modulos/tipovehiculo/tipo-vehiculo.module.ts
+++ src/modulos/tipovehiculo/tipo-vehiculo.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TipoVehiculoController } from './controllers/tipo-vehiculo.controller';
 import { TipoVehiculoService } from './services/tipo-vehiculo.service';
+import { TipoVehiculo } from './entities/tipoVehiculo.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TipoVehiculo]),  
+  ],
   controllers: [TipoVehiculoController],
   providers: [TipoVehiculoService]
 })
