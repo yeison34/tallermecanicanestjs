1ce7e7a35830569d1e91fc478804096817b22a93
Valeria Alarcón <valerialarcon119@gmail.com>
2024-10-17 15:06:51 -0500

    Crud tipo vehiculo

--- src/modulos/tipovehiculo/controllers/tipo-vehiculo.controller.ts
+++ src/modulos/tipovehiculo/controllers/tipo-vehiculo.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { TipoVehiculoService } from '../services/tipo-vehiculo.service';
+import { TipoVehiculo } from '../entities/tipoVehiculo.entity';
 
 @Controller('tipo-vehiculo')
-export class TipoVehiculoController {}
+export class TipoVehiculoController {
+    
+    constructor(
+        private tipoVehiculoService: TipoVehiculoService
+    ) {}
+
+    @Get()
+    ConsultarTiposVehiculo() {
+       return this.tipoVehiculoService.ConsultarTiposVehiculo();
+    }
+
+    @Post()
+    InsertarTipoVehiculo(@Body() tipoVehiculo: TipoVehiculo) {
+        try {
+            return this.tipoVehiculoService.InsertarTipoVehiculo(tipoVehiculo);
+        } catch (ex) {
+            throw ex;
+        }
+    }
+
+    @Put(':id')
+    ActualizarTipoVehiculo(@Param('id') id: number, @Body() tipoVehiculo: TipoVehiculo) {
+        try {
+            return this.tipoVehiculoService.ActualizarTipoVehiculo(id, tipoVehiculo);
+        } catch (ex) {
+            throw ex;
+        }
+    }
+}
