727cb980e9b2097c0c451bade056316426d36334
yissacodev <yesid9523@gmail.com>
2024-10-18 00:44:38 -0500

    Controlador y servicios de Vehiculo corregidos

--- src/modulos/vehiculo/controllers/vehiculo.controller.ts
+++ src/modulos/vehiculo/controllers/vehiculo.controller.ts
@@ -1,37 +1,36 @@
-
-import { Body, Controller,Get, Param, Post,Put,Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post,Put, Delete } from '@nestjs/common';
 import { VehiculoService } from '../services/vehiculo.service';
 import { Vehiculo } from '../entities/vehiculo.entity';
 
 @Controller('vehiculo')
 export class VehiculoController {
     constructor(
-        private vehiculoService:VehiculoService
+        private readonly vehiculoService:VehiculoService
     ){}
 
     @Get()
     ConsultarVehiculos(){
-       return this.vehiculoService.ConsultarVehiculos() 
+       try{
+            return this.vehiculoService.ConsultarVehiculos() 
+        }
+        catch(ex){
+            throw ex;
+        }
     }
 
-    @Get(":id")
-    ConsultarUnoVehiculo(@Param('id') id:number){
-       return this.vehiculoService.EncontrarUno(id) ;
-    }
-    
     @Post()
     InsertarVehiculo(@Body() vehiculo:Vehiculo){
         try{
             return this.vehiculoService.InsertarVehiculo(vehiculo)
-        } catch(ex){
+        }catch(ex){
             throw ex
         }
     }
-    
+
     @Put(":id")
     ActulizarVehiculo(@Param('id') id:number,@Body() vehiculo:Vehiculo){
         try{
-            return this.vehiculoService.ActualizarVehiculo(id,vehiculo)   
+            return this.vehiculoService.ActualizarVehiculo(id, vehiculo)   
         }catch(ex){
             throw ex
         }
@@ -40,11 +39,10 @@ export class VehiculoController {
     @Delete(":id")
     EliminarVehiculo(@Param('id') id:number){
         try{
-            return this.vehiculoService.EliminarVehiculo(id)   
+            return this.vehiculoService.EliminarVehiculo(id);
         }catch(ex){
             throw ex
         }
     }
+
 }
-
-
--- src/modulos/vehiculo/vehiculo.module.ts
+++ src/modulos/vehiculo/vehiculo.module.ts
@@ -1,14 +1,40 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { VehiculoController } from './controllers/vehiculo.controller';
-import { VehiculoService } from './services/vehiculo.service';
-import { Vehiculo} from './entities/vehiculo.entity'
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm'
+import { Repository } from 'typeorm';
+import { Vehiculo } from './entities/vehiculo.entity';
 
-@Module({
-  imports: [
-    TypeOrmModule.forFeature([Vehiculo])
-  ],
-  controllers: [VehiculoController],
-  providers: [VehiculoService]
-})
-export class VehiculoModule {}
+@Injectable()
+export class VehiculoService {
+    constructor(
+        @InjectRepository(Vehiculo) private vehiculoRepository:Repository<Vehiculo>
+    ){}
+
+    ConsultarVehiculos():Promise<Vehiculo[]>{
+        return this.vehiculoRepository.find()
+    }
+
+    InsertarVehiculo(Vehiculo:Vehiculo):Promise<Vehiculo>{
+        const VehiculoCreate = this.vehiculoRepository.create(Vehiculo)
+        return this.vehiculoRepository.save(VehiculoCreate)
+    }
+
+    async ActualizarVehiculo(id: number, vehiculo: Vehiculo):Promise<Vehiculo>{
+        const VehiculoActualizar =  await this.vehiculoRepository.findOne({where: { id }})
+        if(!VehiculoActualizar){
+           throw new NotFoundException(`Vehiculo con ID: ${id} no encontrado`)     
+        }
+        this.vehiculoRepository.merge(VehiculoActualizar, vehiculo);
+        return this.vehiculoRepository.save(VehiculoActualizar)
+    }
+
+    async EliminarVehiculo(id: number):Promise<Vehiculo>{
+        const vehiculoElimnar = await this.vehiculoRepository.findOne({where:{ id }})
+        if(!vehiculoElimnar){
+           throw new NotFoundException(`Vehiculo con ID: ${id} no encontrado`)     
+        }
+        this.vehiculoRepository.remove(vehiculoElimnar);
+        return vehiculoElimnar;
+    }
+
+
+}
