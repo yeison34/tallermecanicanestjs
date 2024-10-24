8344c8ef84f09701b7eb9976ecfcdddb315ac7de
Valeria Alarcón <valerialarcon119@gmail.com>
2024-10-23 23:27:49 -0500

    validaciones creacion vehiculo

--- backend/src/modulos/vehiculo/controllers/vehiculo.controller.ts
+++ backend/src/modulos/vehiculo/controllers/vehiculo.controller.ts
@@ -1,51 +1,60 @@
-
-import { Body, Controller,Get, Param, Post,Put,Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { VehiculoService } from '../services/vehiculo.service';
-import { Vehiculo } from '../entities/vehiculo.entity';
 import { VehiculoDto } from '../dto/vehiculo.dto';
 
 @Controller('vehiculo')
 export class VehiculoController {
-    constructor(
-        private vehiculoService:VehiculoService
-    ){}
+  constructor(private vehiculoService: VehiculoService) {}
 
-    @Get()
-    ConsultarVehiculos(){
-       return this.vehiculoService.ConsultarVehiculos() 
-    }
+  @Get()
+  ConsultarVehiculos() {
+    return this.vehiculoService.ConsultarVehiculos();
+  }
 
-    @Get(":id")
-    ConsultarUnoVehiculo(@Param('id') id:number){
-       return this.vehiculoService.EncontrarUno(id) ;
-    }
-    
-    @Post()
-    InsertarVehiculo(@Body() vehiculo:VehiculoDto){
-        try{
-            return this.vehiculoService.InsertarVehiculo(vehiculo)
-        } catch(ex){
-            throw ex
-        }
-    }
-    
-    @Put()
-    ActulizarVehiculo(@Body() vehiculo:VehiculoDto){
-        try{
-            return this.vehiculoService.ActualizarVehiculo(vehiculo)   
-        }catch(ex){
-            throw ex
-        }
-    }
+  @Get(':id')
+  ConsultarUnoVehiculo(@Param('id') id: number) {
+    return this.vehiculoService.EncontrarUno(id);
+  }
 
-    @Delete(":id")
-    EliminarVehiculo(@Param('id') id:number){
-        try{
-            return this.vehiculoService.EliminarVehiculo(id)   
-        }catch(ex){
-            throw ex
-        }
+  @Post()
+  async InsertarVehiculo(@Body() vehiculo: VehiculoDto) {
+    try {
+      return await this.vehiculoService.InsertarVehiculo(vehiculo);
+    } catch (error) {
+      if (error.message === 'Este vehículo ya está registrado.') {
+        throw new HttpException({
+          status: HttpStatus.CONFLICT,
+          error: 'Este vehículo ya está registrado.'
+        }, HttpStatus.CONFLICT);
+      }
+      throw new HttpException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: 'Ocurrió un error al crear el vehículo.'
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  @Put()
+  async ActulizarVehiculo(@Body() vehiculo: VehiculoDto) {
+    try {
+      return await this.vehiculoService.ActualizarVehiculo(vehiculo);
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: 'Ocurrió un error al actualizar el vehículo.'
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Delete(':id')
+  async EliminarVehiculo(@Param('id') id: number) {
+    try {
+      return await this.vehiculoService.EliminarVehiculo(id);
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: 'Ocurrió un error al eliminar el vehículo.'
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
-
-
